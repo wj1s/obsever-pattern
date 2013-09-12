1608bf8dcf64e3be028492389997fb25cd32e9ea
like <kli@thoughtworks.com>
2013-09-12 17:11:55 +0300

    step4

--- ObserverPattern/WeatherData.cs
+++ ObserverPattern/WeatherData.cs
@@ -7,23 +7,27 @@ namespace ObserverPattern
         private readonly ReapingMachine reapingMachine;
         private readonly SeedingMachine seedingMachine;
         private readonly WateringMachine wateringMachine;
+        private List<IObsever> obsevers = new List<IObsever>();
 
         public WeatherData(SeedingMachine seedingMachine, ReapingMachine reapingMachine, WateringMachine wateringMachine)
         {
             this.seedingMachine = seedingMachine;
             this.reapingMachine = reapingMachine;
             this.wateringMachine = wateringMachine;
+            GetObservers();
+
         }
 
         public void MeasurementsChanged(int temp, int humidity, int windPower)
         {
-            var obsevers = new List<IObsever>();
+            obsevers.ForEach(o=>o.Update(temp,humidity,windPower));
+        }
 
+        private void GetObservers()
+        {
             obsevers.Add(new SeedingMachineObsever(seedingMachine));
             obsevers.Add(new ReapingMachineObserver(reapingMachine));
             obsevers.Add(new WateringMachineObserver(wateringMachine));
-
-            obsevers.ForEach(o=>o.Update(temp,humidity,windPower));
         }
     }
 }
